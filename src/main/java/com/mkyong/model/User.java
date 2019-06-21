ba43ec79f820c90f1e226cea84fc3ebd3c152bd4
Ha Than <ha.than@evizi.com>
2019-06-21 10:49:20 +0700

    Setup and configured Spring Security 5.0 with hsqldb

---
+++ src/main/java/com/mkyong/model/User.java
@@ -0,0 +1,41 @@
+package com.mkyong.model;
+
+public class User {
+
+//	Integer id;
+//	String name;
+//	String email;
+	String username;
+	String password;
+	Integer enabled;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Integer enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + ", enabled=" + enabled + "]";
+	}
+
+}
